e3af1ee66dff3799183a6a39f496492e13a71e5d
pakhuong <anhkhuong1602@gmail.com>
2023-02-18 14:20:47 +0700

    Initial commit

---
+++ program/func_private.py
@@ -0,0 +1,130 @@
+from datetime import datetime, timedelta
+from func_utils import format_number
+import time
+import json
+
+from pprint import pprint
+
+
+# Get existing open positions
+def is_open_positions(client, market):
+
+    # Protect API
+    time.sleep(0.2)
+
+    # Get positions
+    all_positions = client.private.get_positions(
+        market=market,
+        status="OPEN"
+    )
+
+    # Determine if open
+    if len(all_positions.data["positions"]) > 0:
+        return True
+    else:
+        return False
+
+
+# Check order status
+def check_order_status(client, order_id):
+    order = client.private.get_order_by_id(order_id)
+    if order.data:
+        if "order" in order.data.keys():
+            return order.data["order"]["status"]
+    return "FAILED"
+
+
+# Place market order
+def place_market_order(client, market, side, size, price, reduce_only):
+    # Get Position Id
+    account_response = client.private.get_account()
+    position_id = account_response.data["account"]["positionId"]
+
+    # Get expiration time
+    server_time = client.public.get_time()
+    expiration = server_time.data["epoch"] + 70
+
+    # Place an order
+    placed_order = client.private.create_order(
+        position_id=position_id,  # required for creating the order signature
+        market=market,
+        side=side,
+        order_type="MARKET",
+        post_only=False,
+        size=size,
+        price=price,
+        limit_fee='0.015',
+        expiration_epoch_seconds=expiration,
+        time_in_force="FOK",
+        reduce_only=reduce_only
+    )
+
+    # print(placed_order.data)
+
+    # Return result
+    return placed_order.data
+
+
+# Abort all open positions
+def abort_all_positions(client):
+
+    # Cancel all orders
+    client.private.cancel_all_orders()
+
+    # Protect API
+    time.sleep(0.5)
+
+    # Get markets for reference of tick size
+    markets = client.public.get_markets().data
+
+    # Protect API
+    time.sleep(0.5)
+
+    # Get all open positions
+    positions = client.private.get_positions(status="OPEN")
+    all_positions = positions.data["positions"]
+
+    # Handle open positions
+    close_orders = []
+    if len(all_positions) > 0:
+
+        # Loop through each position
+        for position in all_positions:
+
+            # Determine Market
+            market = position["market"]
+
+            # Determine Side
+            side = "BUY"
+            if position["side"] == "LONG":
+                side = "SELL"
+
+            # Get Price
+            price = float(position["entryPrice"])
+            accept_price = price * 1.7 if side == "BUY" else price * 0.3
+            tick_size = markets["markets"][market]["tickSize"]
+            accept_price = format_number(accept_price, tick_size)
+
+            # Place order to close
+            order = place_market_order(
+                client,
+                market,
+                side,
+                position["sumOpen"],
+                accept_price,
+                True
+            )
+
+            # Append the result
+            close_orders.append(order)
+
+            # Protect API
+            time.sleep(0.2)
+
+        # Override json file with empty list
+        bot_agents = []
+        with open("bot_agents.json", "w") as f:
+            json.dump(bot_agents, f)
+
+        # Return closed orders
+        return close_orders
---
+++ program/main.py
@@ -0,0 +1,83 @@
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED, PLACE_TRADES, MANAGE_EXITS
+from func_connections import connect_dydx
+from func_private import abort_all_positions
+from func_public import construct_market_prices
+from func_cointegration import store_cointegration_results
+from func_entry_pairs import open_positions
+from func_exit_pairs import manage_trade_exits
+from func_messaging import send_message
+
+
+# MAIN FUNCTION
+if __name__ == "__main__":
+
+    # Message on start
+    send_message("Bot launch successful")
+
+    # Connect to client
+    try:
+        print("Connecting to Client...")
+        client = connect_dydx()
+    except Exception as e:
+        print("Error connecting to client: ", e)
+        send_message(f"Failed to connect to client {e}")
+        exit(1)
+
+    # Abort all open positions
+    if ABORT_ALL_POSITIONS:
+        try:
+            print("Closing all positions...")
+            close_orders = abort_all_positions(client)
+        except Exception as e:
+            print("Error closing all positions: ", e)
+            send_message(f"Error closing all positions {e}")
+            exit(1)
+
+    # Find Cointegrated Pairs
+    if FIND_COINTEGRATED:
+
+        # Construct Market Prices
+        try:
+            print("Fetching market prices, please allow 3 mins...")
+            df_market_prices = construct_market_prices(client)
+        except Exception as e:
+            print("Error constructing market prices: ", e)
+            send_message(f"Error constructing market prices {e}")
+            exit(1)
+
+        # Store Cointegrated Pairs
+        try:
+            print("Storing cointegrated pairs...")
+            stores_result = store_cointegration_results(df_market_prices)
+            if stores_result != "saved":
+                print("Error saving cointegrated pairs")
+                exit(1)
+        except Exception as e:
+            print("Error saving cointegrated pairs: ", e)
+            send_message(f"Error saving cointegrated pairs {e}")
+            exit(1)
+
+    exit(0)
+
+    # Run as always on
+    while True:
+
+        # Place trades for opening positions
+        if MANAGE_EXITS:
+            try:
+                print("Managing exits...")
+                manage_trade_exits(client)
+            except Exception as e:
+                print("Error managing exiting positions: ", e)
+                send_message(f"Error managing exiting positions {e}")
+                exit(1)
+
+        # Place trades for opening positions
+        if PLACE_TRADES:
+            try:
+                print("Finding trading opportunities...")
+                open_positions(client)
+            except Exception as e:
+                print("Error trading pairs: ", e)
+                send_message(f"Error opening trades {e}")
+                exit(1)
