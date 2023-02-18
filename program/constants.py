e678d443d9849ab82590421d1387c3f957c85f15
pakhuong <anhkhuong1602@gmail.com>
2023-02-18 15:39:08 +0700

    fix: do not abort all positions when the bot first run

--- program/constants.py
+++ program/constants.py
@@ -5,7 +5,7 @@ from decouple import config
 MODE = "DEVELOPMENT"
 
 # Close all open positions and orders
-ABORT_ALL_POSITIONS = True
+ABORT_ALL_POSITIONS = False
 
 # Find Cointegrated Pairs
 FIND_COINTEGRATED = True
